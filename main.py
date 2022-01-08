220268d3ce5c928eece994bbda9a39179430c5aa
theLemon505 <mastercujof15@gmail.com>
2022-01-08 22:31:59 +0000

    basic

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ if __name__ == "__main__":
     text = input("please enter initialization command: ")
     if text == "generate":
         init_generator()
+        print("entering plugin")
+        init_plugin()
     elif text == "plugin":
         init_plugin()
         init_plugin()
